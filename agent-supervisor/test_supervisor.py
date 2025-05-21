017587f83616bd4b37d0d6ce25d18c5df003d3db
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2025-05-21 15:39:36 -0300

    Agent supervisor using create_supervisor

---
+++ agent-supervisor/test_supervisor.py
@@ -0,0 +1,20 @@
+from src.supervisor import supervisor
+from src.services.pretty_print import pretty_print_messages
+
+
+if __name__ == "__main__":
+  for chunk in supervisor.stream(
+    {
+      "messages": [
+        {
+            "role": "user",
+            "content": "find US and New York state GDP in 2024. what % of US GDP was New York state?",
+        }
+      ]
+    },
+  ):
+    pretty_print_messages(chunk, last_message=True)
+
+  last_message = chunk["supervisor"]["messages"][-1].content
+
+  print(f"Last message: {last_message}")
